192e80071ffc6e54577501c498cc6e70bb229867
Jamal Al-Matari <jalmatari@gmail.com>
2025-02-19 17:40:20 +0300

    ⚙️ WIP: Push Changes

--- packages/starter-kit/src/components/scroll-top/index.ts
+++ packages/starter-kit/src/components/scroll-top/index.ts
@@ -1,8 +1,4 @@
 export default class ScrollTop extends HTMLElement {
-    constructor() {
-        super();
-    }
-
     connectedCallback() {
         this.innerHTML = '↑';
         this.style.cssText = 'position:fixed;bottom:20px;right:20px;background:#333;color:white;padding:10px;cursor:pointer;';
